7b2ad805abea5c3e9e4d844d28710e576fd8cde0
kristine <kristinek@github.com>
2019-04-01 14:48:48 +0300

    correcting some stuff

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -6,11 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/",
+        features = "src/test/resources/features/Sample1.feature",
         plugin = {"pretty", "html:cucumber-report/html-report",
                 "junit:cucumber-report/junit-report.xml",
                 "json:cucumber-report/json-report.json"},
-        tags = {"@test", "~@bug"},
+        tags = {"~@test", "~@bug"},
         glue = {"stepDefinitions"}
 )
 public class CucumberRunner {
