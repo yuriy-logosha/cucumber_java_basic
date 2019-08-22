1f44246c926ef5a499af1fcabc6f750a209c6fd5
Evgeniy Fedorov <jevgenijs.fjodorovs@gmail.com>
2019-08-22 17:56:53 +0300

    Task 1

--- src/test/java/stepDefinitions/Task1Steps.java
+++ src/test/java/stepDefinitions/Task1Steps.java
@@ -4,8 +4,10 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,31 +18,82 @@ public class Task1Steps {
         this.driver = Hooks.driver;
     }
 
-    @Given("^I am on the google home page$")
-    public void iAmOnTheHomePage() throws Throwable {
-        driver.get("https://www.google.com/?hl=ru");
+    @Given("I am on the calculator home page$")
+    public void iAmOnTheCalculatorHomePage() throws Throwable {
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
     }
 
-    @Then("^I should see search button$")
-    public void iShouldSeeSearchButton() throws Throwable {
-        assertEquals("Поиск в Google",
-                driver.findElement(By.xpath("//div[@class='FPdoLc VlcLAe']//input[@name='btnK']")).getAttribute("value"));
+    @Then("^I enter number too small and press Submit button$")
+    public void iEnterTooSmallNumber() throws Throwable {
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+        WebElement submitButton = driver.findElement(By.xpath("//button[@class='w3-btn w3-orange w3-margin']"));
+
+        inputField.sendKeys("5");
+        submitButton.click();
     }
 
-    /*@And("^I should see home page's input field for search$")
-    public void iShouldSeeHomePageDescription() throws Throwable {
-        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                driver.findElement(By.cssSelector("p")).getText());
-    }
-    @When("^I enter search request: \"([^\"]*)\"$")
-    public void iEnterName(String name) throws Throwable {
-        driver.findElement(By.id("name")).clear();
-        driver.findElement(By.id("name")).sendKeys(name);
+    @And("^I see error message that number is too small$")
+    public void iSeeErrorMessageSmall() throws Throwable {
+        WebElement errorMessage = driver.findElement(By.xpath("//p[@id='ch1_error']"));
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+
+        assertEquals("Number is too small", errorMessage.getText());
+        inputField.clear();
     }
 
-    @When("^I enter search request$")
-    public void iEnterSearchRequest(String name) throws Throwable {
-        driver.findElement(By.id("name")).clear();
-        driver.findElement(By.id("name")).sendKeys(name);
-    }*/
+    @Then("^I enter number too big and press Submit button$")
+    public void iEnterTooBigNumber() throws Throwable {
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+        WebElement submitButton = driver.findElement(By.xpath("//button[@class='w3-btn w3-orange w3-margin']"));
+
+        inputField.sendKeys("111");
+        submitButton.click();
+    }
+
+    @And("^I see error message that number is too big$")
+    public void iSeeErrorMessageBig() throws Throwable {
+        WebElement errorMessage = driver.findElement(By.xpath("//p[@id='ch1_error']"));
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+
+        assertEquals("Number is too big", errorMessage.getText());
+        inputField.clear();
+    }
+
+    @Then("^I enter text and press Submit button$")
+    public void iEnterText() throws Throwable {
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+        WebElement submitButton = driver.findElement(By.xpath("//button[@class='w3-btn w3-orange w3-margin']"));
+
+        inputField.sendKeys("ACCENTURE");
+        submitButton.click();
+    }
+
+    @And("^I see error message that we need number to be entered$")
+    public void iSeeErrorMessageText() throws Throwable {
+        WebElement errorMessage = driver.findElement(By.xpath("//p[@id='ch1_error']"));
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+
+        assertEquals("Please enter a number", errorMessage.getText());
+        inputField.clear();
+    }
+
+    @Then("^I enter correct number and press Submit button$")
+    public void iEnter81() throws Throwable {
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+        WebElement submitButton = driver.findElement(By.xpath("//button[@class='w3-btn w3-orange w3-margin']"));
+
+        inputField.sendKeys("81");
+        submitButton.click();
+    }
+
+    @And("^I see square root calculations results$")
+    public void iSeeSquareRoot() throws Throwable {
+        //WebElement inputField = driver.findElement(By.xpath("//input[@id='numb']"));
+
+        Alert alert = driver.switchTo().alert();
+
+        assertEquals("Square root of 81 is 9.00", alert.getText());
+        //alert.accept();
+        //inputField.clear();
+    }
 }
--- target/test-classes/features/Task1.feature
+++ target/test-classes/features/Task1.feature
@@ -1,7 +1,14 @@
-Feature: First task
+Feature: Square root
   As a test engineer
-  I want to be able to write and execute a simple scenario for google homepage
+  I want to be able to easy calculate a square root
 
-  Scenario: Opening google home page
-    When I am on the google home page
-    Then I should see search button
+  Scenario: Opening calculator home page
+    When I am on the calculator home page
+    Then I enter number too small and press Submit button
+    And I see error message that number is too small
+    Then I enter number too big and press Submit button
+    And I see error message that number is too big
+    Then I enter text and press Submit button
+    And I see error message that we need number to be entered
+    Then I enter correct number and press Submit button
+    And I see square root calculations results
