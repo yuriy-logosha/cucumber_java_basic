531e26f959e084a41990f68922d79d10a5c61a3c
VladlenaSevelova <vsevelova@gmail.com>
2019-08-23 10:49:36 +0300

    Cucumber Task 1

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,7 +6,6 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import java.util.List;
 import java.util.Map;
@@ -124,4 +123,8 @@ public class SampleSteps {
     @And("^I should see Yandex logo$")
     public void iShouldSeeYandexLogo() {
     }
-}
+
+
+
+    }
+
